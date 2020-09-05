2bd24821a9fcf5ff998e7a11d410bb1bad5683d7
akhilmhdh <akhilmhdh@gmail.com>
2020-09-05 23:19:08 +0530

    added pc view of intro section

--- src/pages/HomePage/IntroSection/IntroSection.tsx
+++ src/pages/HomePage/IntroSection/IntroSection.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const IntroSection: React.FC = () => {
     return (
         <div className="flex flex-col h-screen">
-            <div className="mt-8 ml-8 text-2xl font-rubikMed">
+            <div className="mt-8 ml-8 text-2xl font-rubikMed md:text-5xl md:ml-32">
                 Your personalised
                 <br />
                 skill assistant
             </div>
-            <div className="mt-3 ml-8 text-base font-rubik">
+            <div className="mt-3 ml-8 text-base font-rubik md:ml-32 md:text-4xl">
                 Build your career, lorem ipsum
             </div>
         </div>
