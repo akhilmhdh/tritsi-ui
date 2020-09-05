6237642c85a0ddb152334ca3d1c0e76eacd8217f
akhilmhdh <akhilmhdh@gmail.com>
2020-09-05 22:07:42 +0530

    added introsection

--- src/pages/HomePage/IntroSection/IntroSection.tsx
+++ src/pages/HomePage/IntroSection/IntroSection.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const IntroSection: React.FC = () => {
-    return <h1>Intro</h1>;
+    return (
+        <div className="flex flex-col h-screen">
+            <div className="mt-8 ml-8 text-2xl font-rubikMed">
+                Your personalised
+                <br />
+                skill assistant
+            </div>
+            <div className="mt-3 ml-8 text-base font-rubik">
+                Build your career, lorem ipsum
+            </div>
+        </div>
+    );
 };
 
 export default IntroSection;
