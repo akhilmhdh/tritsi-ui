41762d20e6adfbfbd92b1ef90d5a8ab1ecd5b6be
akhilmhdh <akhilmhdh@gmail.com>
2020-12-24 00:22:35 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 module.exports = {
-    purge: ['./src/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-    future: {
-        removeDeprecatedGapUtilities: true,
-    },
+    purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
     theme: {
+        fontFamily: {
+            tesla: ['Tesla', 'sans-serif'],
+            rubik: ['Rubik', 'sans-serif'],
+            rubikMed: ["'Rubik Medium'", 'sans-serif'],
+            rubikBold: ["'Rubik Bold'", 'sans-serif'],
+        },
         extend: {
-            fontFamily: {
-                tesla: ['Tesla', 'sans-serif'],
-                rubik: ['Rubik', 'sans-serif'],
-                rubikMed: ["'Rubik Medium'", 'sans-serif'],
-                rubikBold: ["'Rubik Bold'", 'sans-serif'],
+            colors: {
+                primary: '#D8E0F0',
+                title: '#000052',
+                background: '#E5E5E5',
+                subtitle: '#151627',
             },
         },
     },
-    variants: {},
+    variants: {
+        extend: {},
+    },
     plugins: [],
 };
